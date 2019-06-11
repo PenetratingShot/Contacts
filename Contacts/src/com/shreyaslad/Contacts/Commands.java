a19c3e36fdbd6ea5482755b73105f2bbb206a562
penetratingshot <shadowtemplates@gmail.com>
2019-06-11 15:50:12 -0700

    added a lot of things, I forgot

--- Contacts/src/com/shreyaslad/Contacts/Commands.java
+++ Contacts/src/com/shreyaslad/Contacts/Commands.java
@@ -1,4 +1,44 @@
 package com.shreyaslad.Contacts;
 
+import org.json.simple.JSONObject;
+
+import java.util.Scanner;
+
 public class Commands {
+    public static void help() {
+
+    }
+
+    public static void create() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Creating a Contact\n");
+        System.out.println("Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Email Addresses (separated by a comma): ");
+        String emails = scanner.nextLine();
+        System.out.println("Phone Numbers (separated by a comma): ");
+        String phoneNumbers = scanner.nextLine();
+
+        JSONObject personDetails = new JSONObject();
+        personDetails.put("email", emails);
+        personDetails.put("numbers", phoneNumbers);
+
+        JSONObject personObject = new JSONObject();
+        personObject.put(name, personDetails);
+
+
+    }
+
+    public static void edit() {
+
+    }
+
+    public static void delete() {
+
+    }
+
+    public static void install() {
+
+    }
 }
